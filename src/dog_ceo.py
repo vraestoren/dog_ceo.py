99239b10e0b76b7787dea28a97e6cdd516651109
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-24 12:49:18 +0300

    Update dog_ceo.py

--- src/dog_ceo.py
+++ src/dog_ceo.py
@@ -1,47 +1,37 @@
-from requests import get
-
+from requests import Session
 
 class DogCeo:
 	def __init__(self) -> None:
 		self.api = "https://dog.ceo/api"
-		self.headers = {
-			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
+		self.session = Session()
+		self.session.headers = {
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
 		}
 	
 	def get_all_breeds(self) -> dict:
-		return get(
-			f"{self.api}/breeds/list/all",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/breeds/list/all").json()
 	
 	def get_random_image(self, count: int = 1) -> dict:
-		return get(
-			f"{self.api}/breeds/image/random/{count}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/breeds/image/random/{count}").json()
 	
 	def get_images_from_breed(self) -> dict:
-		return get(
-			f"{self.api}/breed/hound/images",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/breed/hound/images").json()
 	
 	def get_random_breed_image(self, count: int = 1) -> dict:
-		return get(
-			f"{self.api}/breed/hound/images/random/{count}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/breed/hound/images/random/{count}").json()
 	
 	def get_all_sub_breeds(self) -> dict:
-		return get(
-			f"{self.api}/breed/hound/list",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/breed/hound/list").json()
 	
 	def get_all_sub_breed_images(self) -> dict:
-		return get(
-			f"{self.api}/breed/hound/afghan/images",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/breed/hound/afghan/images").json()
 	
 	def get_random_sub_breed_image(
 			self,
 			count: int = 1) -> dict:
-		return get(
-			f"{self.api}/breed/hound/afghan/images/random/{count}",
-			headers=self.headers).json()
-	
+		return self.session.get(
+			f"{self.api}/breed/hound/afghan/images/random/{count}").json()
